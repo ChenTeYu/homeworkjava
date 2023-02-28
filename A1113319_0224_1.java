3177a8ce5c9bb674fcb1a8cd017c49e7d151df36
ChenTeYu <125658549+ChenTeYu@users.noreply.github.com>
2023-02-28 19:56:46 +0800

    Add files via upload
    230224 Homework
    Page 4-43
    Question 9 & 10

---
+++ A1113319_0224_1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class A1113319_0224_1{
+    public static void main (String [] argv){
+        int Number,Calculate;
+        System.out.println ("請輸入任意整數");
+        Scanner sc = new Scanner(System.in);
+        Number = sc.nextInt();
+        Calculate = Number % 2;
+        if(Calculate == 1){
+		        System.out.println ("該數為奇數");
+			}else{
+		        System.out.println ("該數為偶數");
+			}    
+			
+    }
+}
---
+++ A1113319_0224_2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class A1113319_0224_2{
+    public static void main (String [] argv){
+        Float C,F;
+        System.out.println ("請輸入一攝氏溫度的值");
+        Scanner sc = new Scanner(System.in);
+        C = sc.nextFloat();
+        F = C*9/5+32;
+		   System.out.println ("轉換成華式 = " + F);    
+			
+    }
+}
